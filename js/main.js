3c69da291f6f5e4d3b4403e582b140da38361692
AdhikaryGautam <72125721+AdhikaryGautam@users.noreply.github.com>
2023-08-27 11:45:51 +0545

    final changes

--- js/main.js
+++ js/main.js
@@ -33,3 +33,51 @@ searchBar.addEventListener("mouseout", shrink);
 
 window.addEventListener("load", handleNews);
 newsToggle.addEventListener("click", handleNews);
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  let fullName = document.querySelector("#fullname");
+  let email = document.querySelector("#email");
+  let passFirst = document.querySelector("#password-1");
+  let passSecond = document.querySelector("#password-2");
+
+  // validate
+  if (fullName.value.length === 0) {
+    alert("Full name empty");
+    return;
+  }
+  if (email.value.length === 0) {
+    alert("Email empty");
+    return;
+  } else {
+    if (
+      !email.value.match(
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+      )
+    ) {
+      alert("Enter a valid email");
+      return;
+    }
+  }
+  if (passFirst.value.length === 0) {
+    alert("Password cannot be empty");
+    return;
+  }
+  if (passSecond.value.length === 0) {
+    alert("Password cannot be empty");
+    return;
+  }
+  if (passFirst.value !== passSecond.value) {
+    alert("Both password must be same");
+    return;
+  } else {
+    if (passFirst.value.length < 8) {
+      alert("Password length must be at least 8");
+      return;
+    }
+  }
+  alert("Successfully submitted");
+};
+
+let form = document.querySelector("form");
+form.addEventListener("submit", handleSubmit);
